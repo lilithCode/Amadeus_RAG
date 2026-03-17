86297e0d909c40542078935a8d10b36f8bfbf4ef
lilithCode <hamnamubarak.contact@gmail.com>
2026-03-17 11:24:03 +0500

    Fix: General Prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
 
     const HF_TOKEN = process.env.HF_TOKEN;
 
-    const SPACE_URL = "https://lillilith-amadeusai.hf.space/chat/";
+    const SPACE_URL = "https://lillilith-amadeus.hf.space/chat/";
 
     const response = await fetch(SPACE_URL, {
       method: "POST",
